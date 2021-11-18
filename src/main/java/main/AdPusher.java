6826fac8485a118752feac5bfeb7cee2556bfb63
Hemanthmadduri <saihemu111@gmail.com>
2021-11-18 07:55:36 +0530

    added response paramter for negotation

--- src/main/java/main/AdPusher.java
+++ src/main/java/main/AdPusher.java
@@ -49,16 +49,18 @@ public class AdPusher  {
             List<Integer> adsTobeRemoved =  new ArrayList<>();
 
             for (int k=0; k < adRes.size(); k++) {
-                List<Integer> events = MySQLAccess.dbObj.getEventsforAd(adRes.get(k).merchantid,adRes.get(k).Id);
-                for (int eventId:
-                        events){
-                    EventPayload eventPayload = MySQLAccess.dbObj.getMerchantEvent(adRes.get(k).merchantid,eventId);
-                    System.out.println("checking event for merchant "+adRes.get(k).merchantid +"with event "+eventId);
-                    for (int i = 0; i < lastReceivedAdDetails.length; i++) {
-                        System.out.println("checking for "+lastReceivedAdDetails[i].UserID +"with condition "+eventPayload.EventCondition);
-                        if (!MySQLAccess.dbObj.CheckIfAdValidForUser(lastReceivedAdDetails[i].UserID, eventPayload.EventCondition)) {
+                if(adRes.get(k).Id!=-1) {
+                    List<Integer> events = MySQLAccess.dbObj.getEventsforAd(adRes.get(k).merchantid, adRes.get(k).Id);
+                    for (int eventId :
+                            events) {
+                        EventPayload eventPayload = MySQLAccess.dbObj.getMerchantEvent(adRes.get(k).merchantid, eventId);
+                        System.out.println("checking event for merchant " + adRes.get(k).merchantid + "with event " + eventId);
+                        //for (int i = 0; i < lastReceivedAdDetails.length; i++) {
+                        System.out.println("checking for " + lastReceivedAdDetails[0].UserID + "with condition " + eventPayload.EventCondition);
+                        if (!MySQLAccess.dbObj.CheckIfAdValidForUser(lastReceivedAdDetails[0].UserID, eventPayload.EventCondition)) {
                             adsTobeRemoved.add(adRes.get(k).Id);
                         }
+                        //}
                     }
                 }
             }
